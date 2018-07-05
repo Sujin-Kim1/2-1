e4a75fedd64280907a3ea33b111b8bedd85705ab
Sujin Kim <sujinsu98@gmail.com>
2018-07-05 23:02:08 +0900

    Delete firstPage.js

--- firstPage.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function() {
-    $("#animate").click(function(){
-      $("#animate-demo").animate({backgroundColor: "#aa0000", left:"170px",opacity:"0.5"}, 2000);
-      $("#animate-demo").animate({fontSize:"5em"}, 1000);
-    });
-  });
-  
